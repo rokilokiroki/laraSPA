427aa559ad0e353b9533eb1f83a6f5bbb8df9edb
hiro yama <>
2018-06-04 01:02:55 +0900

    new branch

---
+++ www/laravel/resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/app.css">
+
+        <title>Vue Sample</title>
+    </head>
+    <body>
+        <div id="app">
+            <example></example>
+        </div>
+    </body>
+    <script src="./js/app.js"></script>
+</html>
+
